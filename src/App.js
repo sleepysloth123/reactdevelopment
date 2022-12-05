d5dcb524bd98c3e4384f24a364f72478b89019e4
sleepysloth123 <115028202+sleepysloth123@users.noreply.github.com>
2022-12-05 02:54:27 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,140 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react";
+// contains bakery data and npasses each bakery item to the BakeryItem component
+
+import Navigation from './components/Navigation';
+import ShopItem from './components/ShopItem';
+import menuData from './menu-data.json';
+
+/* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
+menuData.forEach((item) => {
+  item.image = process.env.PUBLIC_URL + "/" + item.image;
+});
+/* ############################################################## */
 
 function App() {
+  const [sortBy, setSort] = React.useState("none");
+  const [categorizeBy, setCategory] = React.useState("All");
+  const [filterList, setFilters] = React.useState([]);
+
+
+  const sortItems = () =>{
+    if (sortBy == "price"){
+      menuData.sort((a,b) => {
+        return a.price - b.price;
+      })
+    } else if (sortBy == "calories"){
+      menuData.sort((a,b) => {
+        return a.calories - b.calories;
+      })
+    }
+  }
+
+  const categorizeItems = (item) =>{
+    if (categorizeBy === "All"){
+      return true;
+    } else if (categorizeBy === item.category) {
+      // menuData.filter((item) => {
+      //   return item.category === categorizeBy;
+      // });
+      return true;
+    } else {
+      return false;
+    }
+    
+  }
+
+  const matchesFilterType = (item) => {
+    if (filterList.length == 0){ // it contains only "All"
+      return true;
+    } else { 
+      for (var i = 0; i < filterList.length; i++){
+        if (!item.description.includes(filterList[i])){
+          return false;
+        } 
+      }
+      return true;
+    }
+  }
+
+  // const categorizedData = menuData.filter((item) => {
+  //   return item.category === categorizeBy || categorizeBy === "All";
+  // });
+  const categorizedData = menuData.filter(categorizeItems);
+  const filteredData = categorizedData.filter(matchesFilterType);
+
+
+  const [cartDict, setCartDict] = React.useState({}); // maps name --> count
+  const [priceDict, setPriceDict] = React.useState({}); // maps name --> one unit price
+  const [totalPrice, setPrice] = React.useState(0);
+
+  const addToPriceDict = (item, price) => {
+    // creates a new item that maps to its price
+    priceDict[item] = price;
+  }
+
+  const addToCart = (item) => {
+    setPrice(totalPrice + priceDict[item]);
+
+    const newCart = {...cartDict};
+    newCart[item] = (newCart[item] || 0) + 1;
+    setCartDict(newCart); 
+  }
+
+  
+  const removeFromCart = (item) => {
+    // removes from totalPrice with number of items*price
+    setPrice(totalPrice - cartDict[item]*priceDict[item]);
+
+    // removes item from cart entirely
+    const newCart = {...cartDict};
+    delete newCart[item]; 
+    setCartDict(newCart);
+  }
+
+  const subtractFromCart = (item) => {
+    // removes from totalPrice with number of items*price
+    setPrice(totalPrice - priceDict[item]);
+
+    cartDict[item] = (cartDict[item] - 1);
+    if (cartDict[item] === 0){
+      delete cartDict[item];
+    }
+ 
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body">
+      <div className="header">
+        <img src="bearkery.png" style={{maxHeight: '100%'}}></img>
+      </div>
+      <div className="main-container">
+        <div className="navigation">
+          <Navigation sortBy={sortBy} 
+          setSort={setSort}
+          setCategory={setCategory} 
+          filterList={filterList} 
+          setFilters={setFilters}
+          cartDict = {cartDict} 
+          totalPrice = {totalPrice}
+          addToCart = {addToCart}
+          subtractFromCart = {subtractFromCart}/>
+        </div>
+        <div className="menu-container">
+          
+          {sortItems()}
+
+          {filteredData.map((item) => (
+            <ShopItem item={item}
+                      addToCart = {addToCart}
+                      addToPriceDict = {addToPriceDict}
+                      priceDict = {priceDict}
+                      removeFromCart = {removeFromCart}
+                      setPrice = {setPrice}
+                      totalPrice = {totalPrice}/>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
